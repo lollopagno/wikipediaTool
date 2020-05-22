762c80ade7108d55348b1df9ce3b2546c3b62c69
albianto97 <email@github.com>
2020-05-22 19:06:35 +0200

    Primi approcci attori

---
+++ mastermind/Mastermind/src/actors/messages/StartTurn.java
@@ -0,0 +1,24 @@
+package actors.messages;
+
+import info.PlayerInfo;
+
+import java.util.List;
+
+public class StartTurn implements Message {
+    private final int length;
+    private final List<PlayerInfo> players;
+
+    public StartTurn(int length, List<PlayerInfo> players)
+    {
+        this.length = length;
+        this.players = players;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public List<PlayerInfo> getPlayers() {
+        return players;
+    }
+}
